f76d3889af1bf67509b1835474cda0614b114242
Mynk-9 <Mynk-9@users.noreply.github.com>
2020-08-05 16:43:01 +0530

    Created class WallpaperHandler
    Static WallpaperHandler class created to handle downloaded wallpapers.

---
+++ wallpepper/Views/WallpaperHandler.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml.Media.Imaging;
+
+namespace wallpepper.Views
+{
+    static class WallpaperHandler
+    {
+        private static BitmapImage SpotlightImage;
+        private static BitmapImage BingImage;
+
+        public static bool isSpotlightImageLoaded { get; private set; } = false;
+        public static bool isBingImageLoaded { get; private set; } = false;
+
+        public static void setSpotlightImage(BitmapImage img)
+        {
+            SpotlightImage = img;
+            isSpotlightImageLoaded = true;
+        }
+        public static void setBingImage(BitmapImage img)
+        {
+            SpotlightImage = img;
+            isBingImageLoaded = true;
+        }
+    }
+}
